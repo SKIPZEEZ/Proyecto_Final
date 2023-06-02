656a77a919be71b9b3c7b9d0dea3a2e055c5e6dc
DiegoVegaProgra <90019265+JuanDiego21ing@users.noreply.github.com>
2023-06-02 00:47:06 -0700

    Add files via upload
    prueba

---
+++ src/main.java
@@ -0,0 +1,14 @@
+
+public class main {
+	
+	public static void main(String[] args) {
+		  double epsilon = 1;
+		  while(epsilon + 1 > 1)
+		  {
+		   epsilon = epsilon/2;
+		  }
+		  epsilon = epsilon *2;
+		  System.out.println(epsilon);
+		 
+		 }
+		}
